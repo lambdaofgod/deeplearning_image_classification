35508f85d4807045781ae2b24876828baee1a8e0
Jakub Bartczuk <bartczukkuba@gmail.com>
2020-05-05 23:36:11 +0200

    ORIENTME data

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,5 +1,18 @@
+# FOODC
 mkdir -p data
 for f in train_images.zip test_images.zip train.csv test.csv
 do
-  wget -nc https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/foodc/v0.1/$f
+  wget -nc https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/foodc/v0.1/$f -O data/$f
+done
+
+# Minileaves
+for f in train-images.npy train-labels.npy test-images.npy all_classes.txt
+do
+    wget -nc https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/minileaves/v0.1/$f -O data/$f
+done
+
+# Orientme
+for f in training.tar.gz test.tar.gz sample_submission.csv
+do
+    wget -nc https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/orientme/v0.1/$f -O data/$f
 done
--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,6 +1,7 @@
 mkdir data
 mkdir data/test
 mkdir data/train
-unzip train_images -d data/train
-unzip test_images -d data/test        
-mv train.csv data
+unzip data/train_images -d data/train
+unzip data/test_images -d data/test
+tar xvzf data/training.tar.gz -C data/
+tar xvzf data/test.tar.gz -C data/
